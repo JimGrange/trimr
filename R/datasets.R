022ffdd21816be99ceec9975aaa463a047443121
JimGrange <grange.jim@gmail.com>
2014-12-23 16:22:19 +0000

    initial commit

---
+++ R/datasets.R
@@ -0,0 +1,17 @@
+#------------------------------------------------------------------------------
+#' Example response time data set
+#'
+#' An example data set containing multiple participants' data for a response
+#' time study involving two experimental conditions. The data set also includes
+#' This is a synthetic data set and has no theoretical basis.
+#'
+#' @format A data frame with 20518 rows and 4 variables:
+#' \describe{
+#'     \item{participant}{participant identification number}
+#'     \item{condition}{the experimental condition (2 in this example)}
+#'     \item{rt}{response time, coded in milliseconds}
+#'     \item{accuracy}{accuracy of the response; 1 = correct, 0 = error}
+#'
+#'     }
+"exampleData"
+#------------------------------------------------------------------------------
