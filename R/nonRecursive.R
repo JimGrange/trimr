7876bffdf89796c33f5281965edb8a4e3eaf9cbc
eddjberry <e.d.j.berry@outlook.com>
2017-03-15 22:26:11 +0000

    tested

--- R/nonRecursive.R
+++ R/nonRecursive.R
@@ -34,27 +34,30 @@
 #'
 #' @export
 
-nonRecursive <- function(data, minRT, omitErrors = TRUE, digits = 3){
+nonRecursive <- function(data, minRT,
+                         ppt.var = "participant", cond.var = "condition", rt.var = "rt", acc.var = "accuracy",
+                         omitErrors = TRUE, digits = 3){
 
   # remove errors if the user has asked for it
   if(omitErrors == TRUE){
-    trimmedData <- subset(data, data$accuracy == 1)
+    trimmedData <- data[data[[acc.var]] == 1, ]
   } else {
     trimmedData <- data
   }
 
   # get the list of participant numbers
-  participant <- sort(unique(trimmedData$participant))
+  participant <- unique(data[[ppt.var]])
 
   # get the list of experimental conditions
-  conditionList <- unique(trimmedData$condition)
+  conditionList <- unique(data[, cond.var])
 
-  # trim the data to remove trials below minRT
-  trimmedData <- subset(trimmedData, trimmedData$rt > minRT)
+  # trim the data
+  trimmedData <- trimmedData[trimmedData[[rt.var]] > minRT, ]
 
   # ready the final data set
-  finalData <- matrix(0, nrow = length(participant),
-                      ncol = length(conditionList))
+  # make a df here to preserve ppt column
+  finalData <- as.data.frame(matrix(0, nrow = length(participant),
+                                    ncol = length(conditionList)))
 
   # give the columns the condition names
   colnames(finalData) <- conditionList
@@ -80,12 +83,12 @@ nonRecursive <- function(data, minRT, omitErrors = TRUE, digits = 3){
     for(currCond in conditionList){
 
       # get the relevant data
-      tempData <- subset(trimmedData, trimmedData$participant == currSub &
-                           trimmedData$condition == currCond)
+      tempData <- trimmedData[trimmedData[[ppt.var]] == currSub &
+                                trimmedData[[cond.var]] == currCond, ]
 
 
       # find the average, and add to the data frame
-      finalData[i, j] <- round(nonRecursiveTrim(tempData$rt), digits = digits)
+      finalData[i, j] <- round(nonRecursiveTrim(tempData[[rt.var]]), digits = digits)
 
       # update condition loop counter
       j <- j + 1
